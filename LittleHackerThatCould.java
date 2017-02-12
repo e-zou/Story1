7a679b994ceb444f13eb0b6eb81d7ca94dc1e969
Emily Zou <emily.zou2@gmail.com>
2017-02-11 22:37:23 -0500

    Add files via upload

---
+++ LittleHackerThatCould.java
@@ -0,0 +1,110 @@
+import java.util.Scanner;
+
+public class LittleHackerThatCould {
+	
+
+	public static void main(String[] args) {
+		// The beginning
+		System.out.println("Hello? Hey, I know you’re there, Consciousness. I could really use your help right now… ");
+		delay(1000);
+		System.out.println("You see, I haven’t been having a good time. I don’t know what to do anymore.");
+		delay(1000);
+		System.out.println("Actually, I feel like crying right now. Can you help me out? ");
+		delay(1000);
+		Scanner input = new Scanner(System.in); 
+		System.out.println("Please type in 'yes' or 'no'. : ");
+		String answer = input.nextLine();
+		delay(1000);
+		System.out.println("Your answer is " + answer + ".");
+		delay(1000);
+		if (answer.toLowerCase().equals("yes")) {
+			System.out.println("Great! I really appreciate your help. Even if other people don’t understand, ");
+			delay(1000);
+			System.out.println("at least I have you by my side. ");
+			delay(1000);
+		}
+		else {
+			System.out.println("Oh. I see how it is… I guess you’re probably really busy…");
+			delay(1000);
+			System.out.println("(even though you’re my own conscience?) -cries-");
+			delay(1000);
+		}
+		//input.close();
+		
+		// Elementary school
+		System.out.println("It all started in elementary school... ");
+		delay(1000);
+		System.out.println("It’s my first day of fifth grade and I couldn’t be more happier!");
+		delay(1000);
+		System.out.println("Most of the kids my age have their parents take them to school but my parents are very busy");
+		delay(1000);
+		System.out.println("people so I just walk to school.");
+		delay(1000);
+		System.out.println("It’s not all that bad though!");
+		delay(1000);
+		System.out.println("I get to see the neighborhood cats!");
+		delay(1000);
+		System.out.println("Just gotta get ready~ -gets ready-");
+		delay(1000);
+		System.out.println("Okay!");
+		delay(1000);
+		System.out.println("I had a bit of an issue with my bookbag since I poked a hole in it (again)");
+		delay(1000);
+		System.out.println("I had to sew it closed since mommy says bookbags are too expensive.");
+		delay(1000);
+		System.out.println("Always wanted bookbags like my friends had but oh well.");
+		delay(1000);
+		System.out.println("Time to go to school! -goes to school-");
+		delay(1000);
+		System.out.println("You know I named one of the cats Spot?");
+		delay(1000);
+		System.out.println("Wanna know why?");
+		
+		//Scanner input1 = new Scanner(System.in); 
+		System.out.println("Please type in 'yes' or 'no'. (capitalization matters): ");
+		String answer1 = input.nextLine();
+		delay(1000);
+		System.out.println("Your answer is " + answer1 + ".");
+		delay(1000);
+		if (answer1.toLowerCase().equals("yes")) {
+			System.out.println("Because he has a cute white patch on his forehead!");
+			delay(1000);
+			System.out.println("Robbie says that’s a dog name but I don’t care what he says.");
+			delay(1000);
+		}
+		else {
+			System.out.println("Oh.");
+			delay(1000);
+			System.out.println("Well it’s because he has a cute white patch on his forehead!");
+			delay(1000);
+			System.out.println("Robbie says that’s a dog name but I don’t care what he says.");
+			delay(1000);
+			System.out.println("I do what I want! -walks to class-");
+			delay(1000);
+		}
+		
+		
+		System.out.println("Whew!");
+		delay(1000);
+		System.out.println("Almost came late to class!");
+		delay(1000);
+		System.out.println("Luckily I ran and the 'pits' didn't stop me.");
+		delay(1000);
+		System.out.println("I'm faster than the speed of light!");
+		delay(1000);
+		System.out.println("");
+		delay(1000);
+		System.out.println("");
+	 	
+	
+	input.close();
+	}
+	
+	 public static void delay(int millis) {
+	        try {
+	            Thread.sleep(millis);
+	        } catch (InterruptedException exp) {
+	        }
+	    }
+	
+}
